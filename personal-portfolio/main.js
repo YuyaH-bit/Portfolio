3b617cef6205cb20059c39e607c1b7bb1b36fa7e
YuyaH-bit <yuyahamadakasuton@gmail.com>
2023-08-08 18:24:39 +0900

    August 8th, 2023 ヘッダー部分着手・未完成

---
+++ personal-portfolio/main.js
@@ -0,0 +1,28 @@
+//target要素を指定
+const target = document.getElementById('navi');
+
+//マウスが要素上に入った時
+// target.addEventListener('mouseover', () => {
+//   target.style.borderBottom = '5px solid blue';
+//   target.style.transition = '0.5s';
+// }, false);
+
+//マウスが要素上から離れた時
+// target.addEventListener('mouseleave', () => {
+//   target.style.borderBottom = 'none';
+// }, false);
+
+let elems = document.getElementsByClassName('hover_blueLine');
+
+for (let i = 0; i < elems.length; i++){
+	//マウスが要素上に入った時
+  elems[i].addEventListener('mouseover', () => {
+  elems[i].style.borderBottom = '5px solid blue';
+  elems[i].style.transition = '0.5s';
+}, false);
+
+//マウスが要素上から離れた時
+elems[i].addEventListener('mouseleave', () => {
+  elems[i].style.borderBottom = 'none';
+}, false);
+}
